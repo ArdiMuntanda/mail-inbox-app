723398cd7fc4eb6ebd18001f412c141f27dc5db9
Ardi Muntanda <ardimuntanda1@gmail.com>
2022-08-06 15:48:54 -0700

    Fetch data from firebase

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -1,27 +1,49 @@
+import { useState } from 'react';
 import Message from './Message';
 import { Link } from 'react-router-dom';
-const dummyMessages = [
-    { 
-        "subject": "Hi Again",
-        "content": "Just wanted to check on you", 
-        "isRead": true 
-    },
-    { 
-        "subject": "Hi Friend", 
-        "content": "Just wanted to let you know I’ m good",
-        "isRead": false
-    }
- ];
+import { useEffect } from 'react';
 function Inbox() {
-    return (
-        <div>
-            <h2>Messages</h2>
-            <div className='message_container'>
-                { dummyMessages.map((message) => { return (<Message propKey={dummyMessages.indexOf(message)} sujet={message.subject} msg={message.content} readStatus={message.isRead} />); 
-                    }) } 
+    const [isLoading, setLoadingState] = useState(true); 
+    const [loadedMessages, setLoadedMessages] = useState([]);
+
+    useEffect(() => {
+        setLoadedMessages(true);
+        fetch(
+            "https://mail-inbox-app-default-rtdb.firebaseio.com/Jim.json"
+        ).then(response => {
+            return response.json();
+        }).then(data => {
+            const allMsg= [];
+            for (const key in data) {
+                const msg = {
+                    id: key, 
+                    ...data[key]
+                };
+                allMsg.push(msg);
+            }
+            setLoadingState(false);
+            setLoadedMessages(data);
+        });
+    }, []);
+    
+    if (isLoading) {
+        return (
+            <div className='loader'>
+                <img src='/icons/email_loader.gif' alt='loader' />
             </div>
-            <Link className='btn' to='/'>Previous</Link>
-        </div>
-    );
+        );
+    } else {
+        return (
+            <div>
+                <h2>Messages</h2>
+                <div className='message_container'>
+                    { loadedMessages.map((message) => { return (<Message propKey={loadedMessages.indexOf(message)} sujet={message.subject} msg={message.content} readStatus={message.isRead} />); 
+                        }) } 
+                </div>
+                <Link className='btn' to='/'>Previous</Link>
+            </div>
+        );
+    }
 }
+
 export default Inbox;
